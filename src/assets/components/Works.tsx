7c9fa62802eb2f132c16ed0b90a00ca7d6c41392
Gonzalo Torres <gonzalotorres317@gmail.com>
2025-07-11 17:33:57 -0300

    Portfolio terminado

--- src/assets/components/Works.tsx
+++ src/assets/components/Works.tsx
@@ -1,4 +1,3 @@
-import bpa from '/bpa.png'
 import tateti from '/tateti.png'
 import movielist from '/movielist.png'
 import panaderia from '/panaderia.png'
@@ -57,37 +56,6 @@ const Works = () => {
                         </div>
                     </div>
                 </div>
-
-                <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16]
-                            group container rounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
-                    <img src={tateti} className="w-full h-full object-cover" />
-                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-                        <span className='text-2x1 font-bold text-white tracking-wider'>Proyecto 1</span>
-                        <div className='pt-8 text-center'>
-                            <a href="">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                    Ver
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16]
-                            group container rounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative">
-                    <img src={bpa} className="w-full h-full object-cover" />
-                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-                        <span className='text-2x1 font-bold text-white tracking-wider'>Proyecto 1</span>
-                        <div className='pt-8 text-center'>
-                            <a href="">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                    Ver
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
     )
